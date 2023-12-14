caa77bbc7fb9e6e1d72a8e2edce43c4b16d09522
DaniCirnaru <152022324+DaniCirnaru@users.noreply.github.com>
2023-12-14 15:32:18 +0200

    Delete JavaRemoteLearning/week1.p4 directory

--- JavaRemoteLearning/week1.p4/src/main/java/com/nagarro/remotelearning/Connection.java
+++
@@ -1,8 +0,0 @@
-package com.nagarro.remotelearning;
-
-public class Connection {
-    //empty class
-    protected Connection() {
-        // Prevent explicit instantiation outside this class by using a private constructor
-    }
-}

15574dcd9e1bd0ea5c631bfc3ea1cf51cedd2b40
DaniCirnaru <152022324+DaniCirnaru@users.noreply.github.com>
2024-03-22 13:17:02 +0200

    Update ThreadRelayRace.java

--- JavaRemoteLearning/week9.p3/src/main/java/com/nagarro/remotelearning/relayrace/ThreadRelayRace.java
+++ JavaRemoteLearning/week9.p3/src/main/java/com/nagarro/remotelearning/relayrace/ThreadRelayRace.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 public class ThreadRelayRace {
     public static void main(String[] args) {
-        // Create and start the teams
+
+        startRace();
+    }
+
+    private static void startRace() {
         ThreadRaceContext context = new ThreadRaceContext();
         List<ThreadRelayRaceTeam> teams = createTeams(context);
-        context.setTotalTeams(teams.size()); // Set the total number of teams
+        context.setTotalTeams(teams.size());
         startTeams(teams);
-        context.waitForAllTeamsToFinish(); // Wait for all teams to finish before displaying results
-        context.displayResults(); // Display the results
+        context.waitForAllTeamsToFinish();
+        context.displayResults();
     }
 
     private static List<ThreadRelayRaceTeam> createTeams(ThreadRaceContext context) {
         List<ThreadRelayRaceTeam> teams = new ArrayList<>();
-        // Create and add teams to the list
-        // For simplicity, let's create 10 teams with 4 competitors each
         for (int i = 0; i < 10; i++) {
             teams.add(new ThreadRelayRaceTeam("Team " + (i + 1), context));
         }
@@ -25,7 +27,6 @@ public class ThreadRelayRace {
     }
 
     private static void startTeams(List<ThreadRelayRaceTeam> teams) {
-        // Start each team
         for (ThreadRelayRaceTeam team : teams) {
             team.start();
         }
